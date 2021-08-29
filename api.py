3f925d9a49d11203db763d2e1f989c9297aaae2f
nairadethya2208 <88815815+nairadethya2208@users.noreply.github.com>
2021-08-29 15:19:14 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -10,9 +10,9 @@ client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+15017122661',
-                     to='+15558675310'
+                     body="Hello , don't sleep at the wheel",
+                     from_='+14122010763',
+                     to='9188357079'
                  )
 
 print(message.sid)
