c8ea073436e438d772fbbeca99fd79c6f101160a
nairadethya2208 <88815815+nairadethya2208@users.noreply.github.com>
2021-08-29 16:58:18 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -1,18 +1,47 @@
-  
-import os
-from twilio.rest import Client
 
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
-
-account_sid = os.environ['ACaa77ea155c8f85c61940555a67ab9744']
-auth_token = os.environ['90951b1fdb81e6dbd9b73ae37ff10586']
-client = Client(account_sid, auth_token)
-
-message = client.messages \
-                .create(
-                     body="Hello , don't sleep at the wheel",
-                     from_='+14122010763',
-                     to='9188357079'
-                 )
-
-print(message.sid)
+client.calls
+      .create({
+         url: 'http://demo.twilio.com/docs/voice.xml',
+         to: '+14155551212',
+         from: '+15017122661'
+       })
+      .then(call => console.log(call.sid));
+{
+  "account_sid": "ACaa77ea155c8f85c61940555a67ab9744",
+  "annotation": null,
+  "answered_by": null,
+  "api_version": "2010-04-01",
+  "caller_name": null,
+  "date_created": "Tue, 31 Aug 2010 20:36:28 +0000",
+  "date_updated": "Tue, 31 Aug 2010 20:36:44 +0000",
+  "direction": "inbound",
+  "duration": "15",
+  "end_time": "Tue, 31 Aug 2010 20:36:44 +0000",
+  "forwarded_from": "+14122010763",
+  "from": "+15017122661",
+  "from_formatted": "(501) 712-2661",
+  "group_sid": null,
+  "parent_call_sid": null,
+  "phone_number_sid": "PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "price": "-0.03000",
+  "price_unit": "USD",
+  "sid": "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "start_time": "Tue, 31 Aug 2010 20:36:29 +0000",
+  "status": "completed",
+  "subresource_uris": {
+    "notifications": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Notifications.json",
+    "recordings": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings.json",
+    "feedback": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Feedback.json",
+    "feedback_summaries": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/FeedbackSummary.json",
+    "payments": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Payments.json",
+    "events": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Events.json"
+  },
+  "to": "+14155551212",
+  "to_formatted": "(415) 555-1212",
+  "trunk_sid": null,
+  "uri": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json",
+  "queue_time": "1000"
+}
